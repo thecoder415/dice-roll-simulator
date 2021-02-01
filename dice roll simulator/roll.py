6b4901a9a9326da83dcf8785f9900d9301983ba4
rishi <thecoder415@gmail.com>
2021-02-01 16:55:08 +0530

    Initial commit

---
+++ dice roll simulator/roll.py
@@ -0,0 +1,29 @@
+import random 
+  
+  
+x = "j"
+   
+while x == "j": 
+      
+    no = random.randint(1,6) 
+      
+    if no == 1: 
+        print("1") 
+       
+    if no == 2: 
+        print("2") 
+      
+    if no == 3: 
+        print("3") 
+       
+    if no == 4: 
+        print("4") 
+      
+    if no == 5: 
+        print("5") 
+      
+    if no == 6: 
+        print("6") 
+          
+    x=input("press j to roll again and n to exit:") 
+    print("\n") 
